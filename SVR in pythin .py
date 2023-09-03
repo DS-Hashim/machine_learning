2492eaa90afd9002eb82211d0c767e56b3d2e063
Hashim4x <hashim.go@gmail.com>
2023-09-03 06:06:08 +0300

    Update SVR in pythin .py

--- SVR in pythin .py
+++ SVR in pythin .py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 # importing the Dataset
 
 
-ataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
@@ -19,4 +19,37 @@ print(X)
 print(y)
 
 
+y = y.reshape(len(y), 1)
 
+print(y)
+
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+print(X)
+print(y)
+
+# Training the SVR model on the whole dataset
+
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1,1))
+
+# Visualising the SVR results
+
+plt.scatter(X, y, color = 'red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1,1)), color = 'blue' )
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
